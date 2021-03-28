96e7eb828dbedae7e340b448f645503db57b7895
pranavjain20 <60300212+pranavjain20@users.noreply.github.com>
2021-03-28 09:14:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 function validateEmail(email) {
 
     var request = new XMLHttpRequest();
-    request.open("GET", "https://phonevalidation.abstractapi.com/v1?api_key=d228273636174d4e82639e8644086efd&phone=14152007986" + email, true);
+    request.open("GET", "https://phonevalidation.abstractapi.com/v1?api_key=d228273636174d4e82639e8644086efd&phone=14152007986" + number, true);
     request.onreadystatechange = function() {
         if (request.readyState == 4 && request.status == 200)
         var result = request.responseText;
         data = JSON.parse(result);
-        console.log(data.is_valid_format.value);
-        if(data.autocorrect != "") {
-            $("#autocorrect").text("Did you mean " + data.autocorrect + "?");
-        }
-        if (data.is_valid_format.value) {
+        console.log(data.valid.value);
+     
+        if (data.valid.value) {
             $("#valid").text(email + " is a valid email address");
-            $("#email").css("box-shadow", "0px 0px 5px rgb(220, 247, 205)");
 
 
         } else {
             $("#valid").text(email + " is an invalid email address");
-            $("#email").css("box-shadow", "0px 0px 5px rgb(247, 211, 205)");
         }
     }
     request.send();
@@ -27,11 +23,7 @@ function validateEmail(email) {
 
 $(document).ready(function(){
     $(":button").click(function(){
-        if($("#email").val() == "") {
-            $("#valid").text("Please enter an email to validate");
-            $("#email").css("box-shadow", "0px 0px 5px rgb(247, 211, 205)");
-        } else {
+ 
             validateEmail($("#email").val());
-        }
     });
 });
